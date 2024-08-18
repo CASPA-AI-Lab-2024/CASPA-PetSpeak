5100bd9a19fd3a99b670ebf8454a520725980953
GWang0122 <53408113+GWang0122@users.noreply.github.com>
2024-08-18 13:04:02 -0700

    Update whisper_exp.py

--- whisper_exp.py
+++ whisper_exp.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-audio_file_path = r"C:\Users\EmilyDesktop\PythonProjects\pycode\tts_story2.mp3"
+audio_file_path = r"C:\Users\PythonProjects\pycode\tts_story2.mp3"
 with open(audio_file_path, 'rb') as audio_file:
     # Call the Whisper API to transcribe the audio file
     response = openai.Audio.transcribe(
